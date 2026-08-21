69cb40a85cc65ff4f5f7d21662ac9967c7a4a30c
Parzival <jmsworld12@gmail.com>
2026-08-21 16:41:36 +0200

    initial commit

---
+++ src/main/java/com/example/invoicetrackerbackend/InvoiceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoicetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoicetrackerbackend/InvoiceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
